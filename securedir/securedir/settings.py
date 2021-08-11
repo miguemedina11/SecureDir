6c6190cf584e9d27bea960122c938f2c5d94031a
miguemedina11 <miguemedina2000@gmail.com>
2021-08-11 11:49:40 +0200

    heroku

--- securedir/securedir/settings.py
+++ securedir/securedir/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,5 +124,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Activate Django-Heroku.
+# Configure Django App for Heroku.
+import django_heroku
 django_heroku.settings(locals())
